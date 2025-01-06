a836d2a15809016eb305b5a96892a1dcd4e9b22b
timmdkrtf <contact@mdkrtf.co.id>
2025-01-06 17:00:17 +0700

    revisi footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import { BsWhatsapp } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
-import { BsFacebook } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { MdLocalPhone } from "react-icons/md";
 import LogoSecondary from "../assets/image/2. Apostrof Logo Utama (N).png"
 
 export default function Footer(){
@@ -10,29 +13,51 @@ export default function Footer(){
                 <div className="row justify-content-between">
                     <div className="col-lg-5 col-md-6 col-12">
                         <img src={LogoSecondary} />
-                        <p className="Poppins">bagian dari Penerbit Abdi Bangun Aksara</p>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-12 link-contact Poppins">
+                        <p className="Poppins" style={{marginTop: "-10px"}}>bagian dari Penerbit Abdi Bangun Aksara</p>
                         <ul style={{listStyleType:"none", padding:"0"}}>
+                            <li className="pb-2">
+                                <a href="https://maps.app.goo.gl/8yRLm8ACmjuVgoQy8" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    Jl. Aria Surialaga No.83, Kota Bogor 16119
+                                </a>
+                            </li>
+                            <li className="pb-2">
+                                <a href="tel:+622518632170" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <MdLocalPhone className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    +62 251 8632 170
+                                </a>
+                            </li>
                             <li className="pb-2">
                                 <a href="https://api.whatsapp.com/send?phone=6281288048052" target="_blank" style={{color: "white", textDecoration: "none"}}>
                                     <BsWhatsapp className="me-3 fs-5" style={{ marginTop:"-3px" }} />
-                                    62 812-8804-8052
+                                    +62 812 8804 8052
                                 </a>
                             </li>
                             <li className="pb-2">
-                                <a href="https://www.instagram.com/halakah.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <BsInstagram className="me-3 fs-5" style={{ marginTop:"-3px" }} />
-                                    halakah.co.id
-                                </a>
-                            </li>
-                            <li className="pb-2">
-                                <a href="https://www.facebook.com/profile.php?id=61561826281024" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <BsFacebook className="me-3 fs-5" style={{ marginTop:"-3px" }} />
-                                    Halakah
+                                <a href="mailto:contact@apostrof.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <MdEmail className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    contact@apostrof.co.id
                                 </a>
                             </li>
                         </ul>
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-12 link-contact Poppins d-flex flex-column justify-content-between">
+                        <div className="d-flex justify-content-end sosmed">
+                            <div className="pb-2 me-2">
+                                <a href="https://www.facebook.com/apostrof.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <FaFacebook className="me-3 fs-3" style={{ marginTop:"-3px" }} />
+                                </a>
+                            </div>
+                            <div className="pb-2 me-2">
+                                <a href="https://www.tiktok.com/@apostrof.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <FaTiktok className="me-3 fs-3" style={{ marginTop:"-3px" }} />
+                                </a>
+                            </div>
+                            <div className="pb-2 me-2">
+                                <a href="https://instagram.com/apostrof.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <BsInstagram className="me-3 fs-3" style={{ marginTop:"-3px" }} />
+                                </a>
+                            </div>
+                        </div>
                         <div className="text-end fst-italic fs-5 mt-4">
                             <p>Jalan Pintas Kerumitan</p>
                         </div>
