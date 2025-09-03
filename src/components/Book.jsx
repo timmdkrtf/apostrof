b02e545d8d7aed9da250449a0b8f62e5e2ad69e6
timmdkrtf <contact@mdkrtf.co.id>
2025-09-03 03:36:33 -0500

    Update Book.jsx
    remove text "segera terbit" in book page

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -14,8 +14,8 @@ export default function Book(){
                             <img src={CoverDepan} alt="" />
                             <img src={CoverBelakang} alt="" />
                         </div>
-                        <h6 className="inria-serif m-0 opacity-75">Segera Terbit!</h6>
-                        <h5 className="roboto">Penyuluh Pertanian: Pejuang Swasembada Pangan</h5>
+                        <h6 className="inria-serif m-0 opacity-75 d-none">Segera Terbit!</h6>
+                        <h5 className="roboto mt-3">Penyuluh Pertanian: Pejuang Swasembada Pangan</h5>
                         <p className="inria-serif">Penyuluh Pertanian: Pejuang Swasembada Pangan mengungkap peran penting penyuluh sebagai penggerak perubahan di balik ketahanan pangan Indonesia. Buku ini memotret perjalanan panjang dunia penyuluhan, dari masa kolonial hingga era digital, lengkap dengan kisah nyata, pendekatan praktis, dan inovasi di lapangan. Sebuah bacaan reflektif dan inspiratif tentang profesi yang sering luput dari sorotan, namun menjadi kunci swasembada dan kedaulatan pangan bangsa.</p>
                         <a className="alike btn" href="/buku/penyuluh-pertanian">Jelajahi Sekarang</a>
                     </div>
